5f941df0ac918d229af11898b14fbc26f31b245a
iwaraich <iwaraich@email.com>
2023-04-21 09:58:11 -0500

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 app.use(express.json());
+app.use( (request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
