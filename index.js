44fb7b0c5aa7e40815b26554bb5e40ba005fe906
iwaraich <iwaraich@email.com>
2023-04-20 21:13:53 -0500

    add first api

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.post('/dependent', (req,res) => {
+    const input = req.body;
+    return res.json(input);
+})
+app.use(function(err, req, res, next) {
+    // 'SyntaxError: Unexpected token n in JSON at position 0'
+    err.message;
+    next(err);
+  });
 app.listen(process.env.PORT || 3000)
